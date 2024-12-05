737358ab3d4575319c5355beea6a89228b5733da
kysrit <102626439+kysrit@users.noreply.github.com>
2024-12-04 21:07:04 -0500

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,39 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+
+const supabase = createClient('https://hztcovrucgaxplyruazp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6dGNvdnJ1Y2dheHBseXJ1YXpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMzNTc0NTUsImV4cCI6MjA0ODkzMzQ1NX0.YUHo4a_g-IENFQIW3zQO4cHU_cAuSFJB-Bm0-ZXrgc0')
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('loginform');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    //formdata
+    const formData = new FormData(form);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    
+    
+    
+    signInWithEmail(email,password)
+
+
+
+  });
+});
+
+async function signInWithEmail(email, password) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password,
+  })
+  if (error) {
+    console.error("Error fetching data:", error);
+    alert('Invalid Credentials! Please Try Again');
+    return null;
+  }else{
+    console.log('Signed in successfully:', data);
+    window.location.href = "/templates/homepage.html";
+  }
+}
---
+++ js/register.js
@@ -0,0 +1,52 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabase = createClient('https://hztcovrucgaxplyruazp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6dGNvdnJ1Y2dheHBseXJ1YXpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMzNTc0NTUsImV4cCI6MjA0ODkzMzQ1NX0.YUHo4a_g-IENFQIW3zQO4cHU_cAuSFJB-Bm0-ZXrgc0')
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('registerform');
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const password = formData.get('password');
+    const confirmPassword = formData.get('confirmPassword');
+    const email = formData.get('email');
+    const confirmEmail = formData.get('confirmEmail');
+
+    if (password !== confirmPassword) {
+      alert('Passwords do not match.');
+      return;
+    }
+
+    if (password.length < 7){
+        alert('Password needs to be longer than 6 characters.');
+      return;
+    }
+
+    if (email !== confirmEmail) {
+      alert('Emails do not match.');
+      return;
+    }
+
+    signUpNewUser(email,password);
+  });
+});
+
+
+async function signUpNewUser(email,password) {
+    const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+        options: {
+            emailRedirectTo: 'https://example.com/welcome',
+        },
+    })
+    if (error) {
+        console.error("Error fetching data:", error);
+        return null;
+    }else{
+        console.log('Signed in successfully:', data);
+        window.location.href = "/templates/login.html";
+    }
+}
